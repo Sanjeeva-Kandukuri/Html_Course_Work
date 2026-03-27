aae20207ef635f2bdbeb2fb0e0abe4d930dfc811
Sanjeev <ksanjeeva225@gmail.com>
2026-03-27 22:39:43 +0530

    Add files via upload

---
+++ crudObjects.js
@@ -0,0 +1,24 @@
+let student = {
+    name : 'Dhanya',
+    age : 25,
+    skills : 'Python Full Stack',
+    details : {
+        salary : '12 LPA' ,
+        company : 'Sai Groups'
+    },
+    savings : [{
+        monthly : '50k'
+    }]
+
+    
+}
+
+// Accessing Values
+
+console.log(student);
+console.log(student.skills);
+console.log(student.details.salary);
+console.log(student.savings[0].monthly);
+
+// updating values
+
